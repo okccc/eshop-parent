c8e0bbc8edf1fab4ac6eb15d5c64d79f759a0848
okccc <1573976179@qq.com>
2024-11-21 10:48:06 +0800

    商品微服务 - RedisTemplate实现缓存

---
+++ common/src/main/java/com/okccc/eshop/common/constant/RedisConstant.java
@@ -0,0 +1,15 @@
+package com.okccc.eshop.common.constant;
+
+/**
+ * @Author: okccc
+ * @Date: 2024/8/3 11:02:00
+ * @Desc: 存储在redis中的常量值
+ */
+public class RedisConstant {
+
+    // 商品微服务：一级分类数据
+    public static final String CATEGORY_ONE = "category:one";
+
+    // 商品微服务：一级分类数据存活时间(天)
+    public static final Integer CATEGORY_ONE_TTL = 7;
+}
--- service/service-product/src/main/java/com/okccc/eshop/product/service/impl/CategoryServiceImpl.java
+++ service/service-product/src/main/java/com/okccc/eshop/product/service/impl/CategoryServiceImpl.java
@@ -1,13 +1,18 @@
 package com.okccc.eshop.product.service.impl;
 
+import com.alibaba.fastjson2.JSON;
+import com.okccc.eshop.common.constant.RedisConstant;
 import com.okccc.eshop.model.entity.product.Category;
 import com.okccc.eshop.product.mapper.CategoryMapper;
 import com.okccc.eshop.product.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author: okccc
@@ -21,11 +26,28 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    /**
+     * 查询一级分类：演示RedisTemplate实现缓存功能
+     */
     @Override
     public List<Category> getOneCategory() {
-        // 查询一级分类
+        // 1.先查缓存,命中就直接返回
+        String value = redisTemplate.opsForValue().get(RedisConstant.CATEGORY_ONE);
+        if (StringUtils.hasText(value)) {
+            log.info("分类管理 - 从redis缓存查询一级分类");
+            List<Category> categoryList = JSON.parseArray(value, Category.class);
+            return categoryList;
+        }
+
+        // 2.没命中就继续查数据库,并将查询结果更新到缓存
         log.info("分类管理 - 从mysql数据库查询一级分类");
         List<Category> categoryList = categoryMapper.selectOneCategory();
+        redisTemplate.opsForValue().set(RedisConstant.CATEGORY_ONE, JSON.toJSONString(categoryList), RedisConstant.CATEGORY_ONE_TTL, TimeUnit.DAYS);
+
+        // 3.返回查询结果
         return categoryList;
     }
 
