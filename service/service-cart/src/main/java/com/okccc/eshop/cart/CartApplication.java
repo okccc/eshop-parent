d75639771bd03353f1ba59ecf198f8707df8e7dd
okccc <1573976179@qq.com>
2024-11-30 11:31:56 +0800

    OpenFeign远程调用和负载均衡

--- service/service-cart/src/main/java/com/okccc/eshop/cart/CartApplication.java
+++ service/service-cart/src/main/java/com/okccc/eshop/cart/CartApplication.java
@@ -1,6 +1,8 @@
 package com.okccc.eshop.cart;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,6 +11,8 @@ import org.springframework.web.client.RestTemplate;
  * @Date: 2024/5/27 10:38:15
  * @Desc:
  */
+@SpringBootApplication
+@EnableFeignClients(basePackages = {"com.okccc.eshop.feign.product"})
 public class CartApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class, args);
--- service/service-cart/src/main/java/com/okccc/eshop/cart/service/impl/CartServiceImpl.java
+++ service/service-cart/src/main/java/com/okccc/eshop/cart/service/impl/CartServiceImpl.java
@@ -1,23 +1,18 @@
 package com.okccc.eshop.cart.service.impl;
 
-import cn.hutool.core.util.RandomUtil;
 import com.alibaba.fastjson2.JSON;
 import com.okccc.eshop.cart.service.CartService;
+import com.okccc.eshop.feign.product.ProductFeignClient;
 import com.okccc.eshop.model.entity.h5.CartInfo;
 import com.okccc.eshop.model.entity.product.ProductSku;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
-import java.util.List;
 
 /**
  * @Author: okccc
@@ -36,6 +31,9 @@ public class CartServiceImpl implements CartService {
 
     @Autowired
     private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private ProductFeignClient productFeignClient;
 
     @Override
     public void save(Long skuId, Integer skuNum) {
@@ -57,25 +55,31 @@ public class CartServiceImpl implements CartService {
             // 根据skuId查询商品sku信息,服务拆分后需要远程调用service-product模块
 //            ProductSku productSku = productService.getBySkuId(skuId);
 
-            // 订阅service-product服务的实例列表
-            List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
-            // 随机挑选一个实例,实现负载均衡
-            ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
-            // 版本1：使用RestTemplate发送http请求
-            ResponseEntity<ProductSku> responseEntity = restTemplate.exchange(
-                    instance.getUri() + "/api/product/getBySkuId/{skuId}",  // 请求路径
-                    HttpMethod.GET,  // 请求方式
-                    null,  // 请求实体,可以为空
-                    new ParameterizedTypeReference<>() {},  // 返回值类型
-                    skuId  // 请求参数
-            );
-            // 解析响应结果
-            if (!responseEntity.getStatusCode().is2xxSuccessful()) {
-                // 查询失败,直接结束
-                return;
-            }
-            // 获取响应体
-            ProductSku productSku = responseEntity.getBody();
+//            // 订阅service-product服务的实例列表
+//            List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
+//            // 随机挑选一个实例,实现负载均衡
+//            ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
+//            // 版本1：使用RestTemplate发送http请求
+//            ResponseEntity<ProductSku> responseEntity = restTemplate.exchange(
+//                    instance.getUri() + "/api/product/getBySkuId/{skuId}",  // 请求路径
+//                    HttpMethod.GET,  // 请求方式
+//                    null,  // 请求实体,可以为空
+//                    new ParameterizedTypeReference<>() {},  // 返回值类型
+//                    skuId  // 请求参数
+//            );
+//            // 解析响应结果
+//            if (!responseEntity.getStatusCode().is2xxSuccessful()) {
+//                // 查询失败,直接结束
+//                return;
+//            }
+//            // 获取响应体
+//            ProductSku productSku = responseEntity.getBody();
+
+            // 版本2：RestTemplate代码过于复杂,而且与原先的本地方法调用差异太大,远程调用应该和本地方法调用一样简单才对
+            // 远程调用的三个关键点：服务名称、请求方式和路径、请求参数和返回值类型
+            // OpenFeign就是利用SpringMVC的相关注解来声明上述信息,然后基于动态代理生成远程调用的代码
+            // OpenFeign替我们完成了服务订阅、负载均衡、发送http请求等所有工作,看起来优雅多了
+            ProductSku productSku = productFeignClient.getBySkuId(skuId);
 
             // 填充购物车相关信息
             cartInfo.setUserId(1L);
