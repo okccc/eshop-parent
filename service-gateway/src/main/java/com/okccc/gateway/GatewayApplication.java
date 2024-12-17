0fec0288f8b11e12368cf84573943af5968a856d
okccc <1573976179@qq.com>
2024-12-17 10:30:20 +0800

    网关路由转发

---
+++ service-gateway/src/main/java/com/okccc/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.okccc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: okccc
+ * @Date: 2024/5/25 10:30:15
+ * @Desc:
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
