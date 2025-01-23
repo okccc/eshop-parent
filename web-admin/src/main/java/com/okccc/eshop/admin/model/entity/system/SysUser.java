4709faaf3af0cebcd005714d59e370ca7d311512
okccc <1573976179@qq.com>
2025-01-23 10:23:56 +0800

    @TableField(select = false)过滤敏感信息

--- web-admin/src/main/java/com/okccc/eshop/admin/model/entity/system/SysUser.java
+++ web-admin/src/main/java/com/okccc/eshop/admin/model/entity/system/SysUser.java
@@ -1,5 +1,6 @@
 package com.okccc.eshop.admin.model.entity.system;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.okccc.eshop.admin.model.entity.base.BaseEntity;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -17,7 +18,9 @@ public class SysUser extends BaseEntity {
 	@Schema(description = "用户名")
 	private String username;
 
+	// 密码属于敏感信息,应该在查询时过滤掉,所以登录校验时通用Mapper查不到password,需要自定义Mapper
 	@Schema(description = "密码")
+	@TableField(select = false)
 	private String password;
 
 	@Schema(description = "昵称")
